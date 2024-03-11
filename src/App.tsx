f64d5a448e535920133f2d0b97a39491a1c8ce6a
L. Gonsalves <lgo@n-side.com>
2024-03-11 19:45:49 +0100

    Playground is set up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import SimVisualization from "./components/SimVisualization.tsx";
 
 function App() {
   return (
-    <div className="flex flex-col h-full w-full gap-1 bg-gray-950 text-gray-500">
-      <div className="p-5">
+    <div className="flex flex-col h-full w-full gap-1 bg-gray-950 text-gray-500 p-7">
+      <div>
         <h1 className="text-gray-400 text-lg">
           EconoMimic v1.0
         </h1>
-        <hr className="border border-gray-900 my-5" />
+        <hr className="border border-transparent my-5" />
+      </div>
+      <div className="grow border rounded-lg border-gray-800 overflow-hidden">
+        <SimVisualization/>
       </div>
     </div>
   )
---
+++ src/components/SimVisualization.tsx
@@ -0,0 +1,5 @@
+function SimVisualization() {
+  return <svg className="bg-gray-900" viewBox="0 0 1000 1000"></svg>;
+}
+
+export default SimVisualization;
